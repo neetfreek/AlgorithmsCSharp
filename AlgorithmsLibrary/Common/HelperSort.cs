bee648aa3a550cdf996617cbdc8cd7fc02f383dc
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-04-06 19:36:10 +0300

    Add char method overloads, improve comments

--- AlgorithmsLibrary/Common/HelperSort.cs
+++ AlgorithmsLibrary/Common/HelperSort.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
+/*======================================================*
+*  Common helper methods for AlgorithmsLibrary sorting  *
+*=======================================================*/
 namespace AlgorithmsLibrary
 {
     public static class HelperSort
     {
-        // Convert matrix row to vector, zero-based row index
+        /*==============================================================*
+        *  Return variables of matrix row in CHARACTER, INTEGER vector  *
+        *===============================================================*/
         public static int[] MatrixRowToVector(int[,] matrix, int row)
         {
             // Set vector length to number of columns
@@ -19,15 +22,55 @@ namespace AlgorithmsLibrary
             }
             return vector;
         }
+        public static char[] MatrixRowToVector(char[,] matrix, int row)
+        {
+            // Set vector length to number of columns
+            char[] vector = new char[matrix.GetLength(1)];
 
+            // Iterate columns
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                vector[col] = matrix[row, col];
+            }
+            return vector;
+        }
+
+        /*==============================================================================*
+        *  Replace specified row in CHARACTER, INTEGER matrix with  vector              *
+        *  Return, print warning, if row doesn't contain same number elements as vector *
+        *===============================================================================*/
         public static void OverWriteMatrixRowWithVector(int[,] matrix, int[] vector, int row)
         {
+            // ensure number of matrix row elements and vector elements match
+            if (matrix.GetLength(1) != vector.Length)
+            {
+                Console.WriteLine("Aborting: number of elements in matrix row and vector don't match.");
+                return;
+            }
+
+            for (int col = 0; col < vector.Length; col++)
+            {
+                matrix[row, col] = vector[col];
+            }
+        }
+        public static void OverWriteMatrixRowWithVector(char[,] matrix, char[] vector, int row)
+        {
+            // ensure number of matrix row elements and vector elements match
+            if (matrix.GetLength(1) != vector.Length)
+            {
+                Console.WriteLine("Aborting: number of elements in matrix row and vector don't match.");
+                return;
+            }
+
             for (int col = 0; col < vector.Length; col++)
             {
                 matrix[row, col] = vector[col];
             }
         }
 
+        /*==============================================*
+        *  Return CHARACTER, INTEGER matrix as vector   *
+        *===============================================*/
         public static int[] MatrixToVector(int[,] matrix)
         {
             // Declare, initialise vector with default (0) values
@@ -48,8 +91,30 @@ namespace AlgorithmsLibrary
 
             return vector;
         }
+        public static char[] MatrixToVector(char[,] matrix)
+        {
+            // Declare, initialise vector with default (0) values
+            char[] vector = new char[matrix.Length];
 
-        // Swaps the values in array at indecies indexFirst, indexSecond
+            int counter = 0;
+
+            // Iterate rows
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {   // Iterate columns (elements)
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    // Assign col value to vector
+                    vector[counter] = matrix[row, col];
+                    counter++;
+                }
+            }
+
+            return vector;
+        }
+
+        /*==========================================*
+        *  Swap CHARACTER, INTEGER values in matrix *
+        *===========================================*/
         public static void Swap(int[] array, int indexFirst, int indexSecond)
         {
             //Console.WriteLine($"{array[indexFirst]}, swapping with with {array[indexSecond]})");
@@ -57,7 +122,6 @@ namespace AlgorithmsLibrary
             array[indexFirst] = array[indexSecond];
             array[indexSecond] = temp;
         }
-
         internal static void Swap(char[] array, int indexFirst, int indexSecond)
         {
             //Console.WriteLine($"{array[indexFirst]}, swapping with with {array[indexSecond]})");
