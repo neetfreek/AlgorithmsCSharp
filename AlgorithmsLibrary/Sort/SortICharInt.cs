5778ee5015d32c2d5da6c2a8d246e61a407eb13c
COOLBEANS\Jonathan <jonathan.widdowson1@gmail.com>
2019-04-06 21:12:39 +0300

    Add overload for char sorting methods, improve comments

--- AlgorithmsLibrary/Sort/SortICharInt.cs
+++ AlgorithmsLibrary/Sort/SortICharInt.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*==================================================================*
+*  Methods for sorting Vectors (1D arrays) and matrices (2D arrays) *
+*  Methods overloaded for CHARACTER, INTEGER arrays                                                                 *
+*===================================================================*/
 
 namespace AlgorithmsLibrary
 {
-    public static class SortInt
+    public static class SortCharInt
     {
-        // Sort elements of vector
+        /*======================================================*
+        *  Selection Sort CHARACTER, INTEGER vector             *
+        *  Vector elements sorted from lowest to highest value  *  
+        *=======================================================*/
         public static void SortSelectionVector(int[] vector)
         {
             int length = vector.Length;
@@ -26,7 +30,6 @@ namespace AlgorithmsLibrary
                 }
             }
         }
-
         public static void SortSelectionVector(char[] vector)
         {
             int length = vector.Length;
@@ -47,7 +50,10 @@ namespace AlgorithmsLibrary
             }
         }
 
-        // Sort elements of matrix
+        /*======================================================*
+        *  Selection Sort CHARACTER, INTEGER matrix             *
+        *  Matrix elements sorted from lowest to highest value  *
+        *=======================================================*/
         public static void SortSelectionMatrix(int[,] matrix)
         {
             // Declare vector, intialise values to those of matrix's
@@ -68,8 +74,31 @@ namespace AlgorithmsLibrary
                 }
             }
         }
+        public static void SortSelectionMatrix(char[,] matrix)
+        {
+            // Declare vector, intialise values to those of matrix's
+            char[] vectorOfMatrix = HelperSort.MatrixToVector(matrix);
+            SortSelectionVector(vectorOfMatrix);
 
-        // Sort elements in each row of matrix
+            int counter = 0;
+
+            // Iterate rows
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                // Iterate columns
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    // Assign current element's value to vector's
+                    matrix[row, col] = vectorOfMatrix[counter];
+                    counter++;
+                }
+            }
+        }
+
+        /*==============================================================*
+        *  Selection Sort CHARACTER, INTEGER matrix's rows              *
+        *  Matrix rows' elements sorted from lowest to highest value    *
+        *===============================================================*/
         public static void SortSelectionMatrixRows(int[,] matrix)
         {
             int length = Matrices.CountElementsMatrix(matrix);
@@ -79,6 +108,19 @@ namespace AlgorithmsLibrary
             {
                 // Return new vector comprised of row's elements
                 int[] vectorRow = HelperSort.MatrixRowToVector(matrix, row);
+                SortSelectionVector(vectorRow);
+                HelperSort.OverWriteMatrixRowWithVector(matrix, vectorRow, row);
+            }
+        }
+        public static void SortSelectionMatrixRows(char[,] matrix)
+        {
+            int length = Matrices.CountElementsMatrix(matrix);
+
+            // Iterate vector rows
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                // Return new vector comprised of row's elements
+                char[] vectorRow = HelperSort.MatrixRowToVector(matrix, row);
                 SortSelectionVector(vectorRow);
                 HelperSort.OverWriteMatrixRowWithVector(matrix, vectorRow, row);
             }
